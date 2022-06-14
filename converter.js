7ba3898f99f9676cf57eeb2ae0f53b71af2698ee
Md. Wahid Sadiq Ratul <63424209+Ratul-netizen@users.noreply.github.com>
2022-06-14 09:13:25 +0600

    Update

--- converter.js
+++ converter.js
@@ -9,32 +9,27 @@
 //         value * 0.1574;
 // }
 
-document.body.style.textAlign = 'center';
-var input = document.getElementById('input');
-var result = document.getElementById('result');
-var inputType = document.getElementById('inputType');
-var resultType = document.getElementById('resultType');
-var inputTypeValue,resultTypeValue;
+document.body.style.textAlign='center'
+const btn = document.querySelector(".btn");
+const input = document.querySelector("input");
+const conversion = document.querySelector("#conversion");
+const span = document.querySelector("span");
 
-input.addEventListener("keyup",myResult);
-inputType.addEventListener("change",myResult);
-resultType.addEventListener("change",myResult);
+btn.addEventListener("click",()=>{
+    let hello = conversion.selectedIndex;
+    let hello2 = input.value;
+    convert(hello,hello2);
+})
 
-inputTypeValue = inputType.value;
-resultTypeValue = resultType.value;
-function myResult(){
-
-  inputTypeValue = inputType.value;
-  resultTypeValue = resultType.value;
- 
-  if(inputTypeValue === "pound" && resultTypeValue === "kilo"){
-
-    result.value = Number(input.value)*0.453;
-
-  }else if(inputTypeValue === "kilo" && resultTypeValue === "pound"){
-
-    result.value = Number(input.value)*2.205;
-
-  }
-  
+const convert = (hello,hello2) =>{
+    if(hello==0){
+        let lb = hello2;
+        let kg = parseFloat(lb) * 0.4536;
+        span.innerHTML = `${kg}`;        
+    }
+    else{
+        let kg = hello2;
+        let lb = parseFloat(kg) * 2.2046;
+        span.innerHTML = `${lb}`;        
+    }   
 }
