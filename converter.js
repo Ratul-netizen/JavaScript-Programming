b6540da104d62a5910fb51ce4e6269374d9fdbb2
Md. Wahid Sadiq Ratul <63424209+Ratul-netizen@users.noreply.github.com>
2022-06-14 08:53:54 +0600

    Add files via upload

--- converter.js
+++ converter.js
@@ -1,10 +1,40 @@
-function kiloConvert(value) {
-    document.getElementById("Pounds").innerHTML =
-        value * 2.2046;
-    document.getElementById("Ounces").innerHTML =
-        value * 35.274;
-    document.getElementById("Grams").innerHTML =
-        value * 1000;
-    document.getElementById("Stones").innerHTML =
-        value * 0.1574;
+// function kiloConvert(value) {
+//     document.getElementById("Pounds").innerHTML =
+//         value * 2.2046;
+//     document.getElementById("Ounces").innerHTML =
+//         value * 35.274;
+//     document.getElementById("Grams").innerHTML =
+//         value * 1000;
+//     document.getElementById("Stones").innerHTML =
+//         value * 0.1574;
+// }
+
+document.body.style.textAlign = 'center';
+var input = document.getElementById('input');
+var result = document.getElementById('result');
+var inputType = document.getElementById('inputType');
+var resultType = document.getElementById('resultType');
+var inputTypeValue,resultTypeValue;
+
+input.addEventListener("keyup",myResult);
+inputType.addEventListener("change",myResult);
+resultType.addEventListener("change",myResult);
+
+inputTypeValue = inputType.value;
+resultTypeValue = resultType.value;
+function myResult(){
+
+  inputTypeValue = inputType.value;
+  resultTypeValue = resultType.value;
+ 
+  if(inputTypeValue === "pound" && resultTypeValue === "kilo"){
+
+    result.value = Number(input.value)*0.453;
+
+  }else if(inputTypeValue === "kilo" && resultTypeValue === "pound"){
+
+    result.value = Number(input.value)*2.205;
+
+  }
+  
 }
